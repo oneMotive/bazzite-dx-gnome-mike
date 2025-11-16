ce432a337e4387a8f03da7db17691ed9d8a02919
Mike Stewart <dungeonmesher@protonmail.com>
2025-11-16 11:46:34 -0500

    oh my zsh

--- build_files/build.sh
+++ build_files/build.sh
@@ -11,7 +11,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y zsh-autosuggestions.noarch
+#dnf5 install -y tmux
 
 # Use a COPR Example:
 #
